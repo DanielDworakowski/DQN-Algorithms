d90313ebbb276b45ac54b72c2f3702c6050a1ccf
Daniel Dworakowski <dworakowskidaniel@gmail.com>
2018-02-24 23:16:23 -0500

    update more things to work with pytorch.

---
+++ deepMindModel.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+class atari_model(torch.nn.Module):
+
+    def __init__(self, num_actions):
+        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, num_actions)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out = self.relu(out)
+        out = out.view(x.shape[0], -1)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
--- run_dqn_atari.py
+++ run_dqn_atari.py
@@ -1,35 +1,15 @@
-import argparse
-import gym
-from gym import wrappers
-import os.path as osp
-import random
-import numpy as np
-import tensorflow as tf
-import tensorflow.contrib.layers as layers
-
 import dqn
+import gym
+import torch
+import random
+import argparse
+import numpy as np
+import os.path as osp
 from dqn_utils import *
+from gym import wrappers
 from atari_wrappers import *
 
-def atari_model(img_in, num_actions, scope, reuse=False):
-    # as described in https://storage.googleapis.com/deepmind-data/assets/papers/DeepMindNature14236Paper.pdf
-    with tf.variable_scope(scope, reuse=reuse):
-        out = img_in
-        with tf.variable_scope("convnet"):
-            # original architecture
-            out = layers.convolution2d(out, num_outputs=32, kernel_size=8, stride=4, activation_fn=tf.nn.relu)
-            out = layers.convolution2d(out, num_outputs=64, kernel_size=4, stride=2, activation_fn=tf.nn.relu)
-            out = layers.convolution2d(out, num_outputs=64, kernel_size=3, stride=1, activation_fn=tf.nn.relu)
-        out = layers.flatten(out)
-        with tf.variable_scope("action_value"):
-            out = layers.fully_connected(out, num_outputs=512,         activation_fn=tf.nn.relu)
-            out = layers.fully_connected(out, num_outputs=num_actions, activation_fn=None)
-
-        return out
-
-def atari_learn(env,
-                session,
-                num_timesteps):
+def atari_learn(env, session, num_timesteps):
     # This is just a rough estimate
     num_iterations = float(num_timesteps) / 4.0
 
@@ -40,11 +20,10 @@ def atari_learn(env,
                                          (num_iterations / 2,  5e-5 * lr_multiplier),
                                     ],
                                     outside_value=5e-5 * lr_multiplier)
-    optimizer = dqn.OptimizerSpec(
-        constructor=tf.train.AdamOptimizer,
-        kwargs=dict(epsilon=1e-4),
-        lr_schedule=lr_schedule
-    )
+    
+    model = atari_model(env.action_space.n)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr_multiplier, betas=(0.9, 0.999), eps=1e-4, weight_decay=0)
+    schedule = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda epoch: lr_schedule(epoch))
 
     def stopping_criterion(env, t):
         # notice that here t is the number of steps of the wrapped env,
@@ -56,58 +35,35 @@ def atari_learn(env,
             (0, 1.0),
             (1e6, 0.1),
             (num_iterations / 2, 0.01),
-        ], outside_value=0.01
-    )
+        ], outside_value=0.01)
 
     dqn.learn(
         env,
-        q_func=atari_model,
-        optimizer_spec=optimizer,
-        session=session,
-        exploration=exploration_schedule,
-        stopping_criterion=stopping_criterion,
-        replay_buffer_size=1000000,
-        batch_size=32,
-        gamma=0.99,
-        learning_starts=50000,
-        learning_freq=4,
-        frame_history_len=4,
-        target_update_freq=10000,
-        grad_norm_clipping=10
+        q_func = atari_model,
+        optimizer_spec = optimizer,
+        session = session,
+        exploration = exploration_schedule,
+        stopping_criterion = stopping_criterion,
+        replay_buffer_size = 1000000,
+        batch_size = 32,
+        gamma = 0.99,
+        learning_starts = 50000,
+        learning_freq = 4,
+        frame_history_len = 4,
+        target_update_freq = 10000,
+        grad_norm_clipping = 10
     )
     env.close()
 
-def get_available_gpus():
-    from tensorflow.python.client import device_lib
-    local_device_protos = device_lib.list_local_devices()
-    return [x.physical_device_desc for x in local_device_protos if x.device_type == 'GPU']
-
-def set_global_seeds(i):
-    try:
-        import tensorflow as tf
-    except ImportError:
-        pass
-    else:
-        tf.set_random_seed(i)
-    np.random.seed(i)
-    random.seed(i)
-
-def get_session():
-    tf.reset_default_graph()
-    tf_config = tf.ConfigProto(
-        inter_op_parallelism_threads=1,
-        intra_op_parallelism_threads=1)
-    session = tf.Session(config=tf_config)
-    print("AVAILABLE GPUS: ", get_available_gpus())
-    return session
-
-def get_env(task, seed):
-    env_id = task.env_id
-
-    env = gym.make(env_id)
-
-    set_global_seeds(seed)
+def setRandomSeeds(seed, env):
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
     env.seed(seed)
+
+def configureEnv(env):
 
     expt_dir = '/tmp/hw3_vid_dir2/'
     env = wrappers.Monitor(env, osp.join(expt_dir, "gym"), force=True)
@@ -116,19 +72,13 @@ def get_env(task, seed):
     return env
 
 def main():
-    # Get Atari games.
-    # benchmark = gym.benchmark_spec('Atari40M')
 
-    # Change the index to select a different game.
-    # task = benchmark.tasks[3]
-
-
-    # Run training
-    seed = 0 # Use a seed of zero (you may want to randomize the seed!)
-    # env = get_env(task, seed)
+    # Run training 
     specs = gym.envs.registry
     env = gym.make('Pong-v0')
-    session = get_session()
+    setRandomSeeds(0, env)
+    configureEnv(env)
+    # env = get_env(task, seed)
     # tmp = specs.env_specs['Pong-v0'].timestep_limit
     atari_learn(env, session, num_timesteps=1e7)
 
